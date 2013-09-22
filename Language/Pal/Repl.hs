6dd70f152c519a7ff257ec5e0250d785618b0c0a
Sam Stokes <me@samstokes.co.uk>
2013-09-22 10:18:55 -0700

    Nicen REPL error handling with wrapper monad

--- Language/Pal/Repl.hs
+++ Language/Pal/Repl.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Language.Pal.Repl (
@@ -8,7 +9,7 @@ module Language.Pal.Repl (
 import Prelude hiding (read)
 import Control.Applicative (Applicative)
 import Control.Error
-import Control.Monad.Trans (lift)
+import Control.Monad.State
 import Text.Parsec (parse, ParseError)
 
 import Language.Pal.Eval (eval)
@@ -19,19 +20,37 @@ import Language.Pal.Types
 data ReplError = ParseError ParseError | EvalError EvalError
   deriving (Show)
 
+
+newtype ReplT m a = ReplT { unReplT :: EitherT ReplError m a }
+  deriving (Monad, Applicative, Functor)
+
+instance MonadTrans ReplT where
+  lift = ReplT . lift
+
+runReplT :: Monad m => ReplT m a -> m (Either ReplError a)
+runReplT = runEitherT . unReplT
+
+liftEither :: Monad m => Either ReplError a -> ReplT m a
+liftEither = ReplT . hoistEither
+
+
 rep :: IO ()
-rep = runEitherT rep' >>= handle where
+rep = runReplT rep' >>= handle where
+  handle :: Either ReplError () -> IO ()
   handle (Right ()) = return ()
   handle (Left e) = print e
+  rep' :: ReplT IO ()
   rep' = do
     input <- lift       getContents
-    e <- hoistEither $  read "<stdin>" input
-    v <- EitherT $      eval' e
+    e <- liftEither $   read "<stdin>" input
+    v <-                eval' e
     lift $              print v
 
 
 read :: FilePath -> String -> Either ReplError LValue
 read src = fmapL ParseError . parse expr src
 
-eval' :: (Applicative m, Monad m) => LValue -> m (Either ReplError LValue)
-eval' = fmap (fmapL EvalError . fst) . eval
+eval' :: (Applicative m, Monad m) => LValue -> ReplT m LValue
+eval' val = do
+  (eOrV, _) <- eval val
+  liftEither $ either (throwE . EvalError) Right eOrV
